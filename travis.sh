86adb4f66587d275c1a421951c203e27c424eb2f
Akimasa Tateba <akimasa@gmail.com>
2019-03-01 18:49:47 +0900

    wii-dev

--- travis.sh
+++ travis.sh
@@ -1,6 +1,7 @@
 export DEVKITPRO=/opt/devkitpro
 export DEVKITARM=/opt/devkitpro/devkitARM
 export DEVKITPPC=/opt/devkitpro/devkitPPC
+dkp-pacman -Syyu --noconfirm wii-dev
 cd /root/kernel/bin2h/
 bash make.sh
 cd /root/

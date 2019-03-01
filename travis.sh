408714f59159987deddc3279b988c4add184cd33
Akimasa Tateba <akimasa@gmail.com>
2019-03-01 18:59:08 +0900

    devkitarm

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 export DEVKITPRO=/opt/devkitpro
 export DEVKITARM=/opt/devkitpro/devkitARM
 export DEVKITPPC=/opt/devkitpro/devkitPPC
-dkp-pacman -Syyu --noconfirm wii-dev
+dkp-pacman -Syyu --noconfirm 3ds-dev nds-dev gp32-dev gba-dev nds-portlibs
 cd /root/kernel/bin2h/
 bash make.sh
 cd /root/

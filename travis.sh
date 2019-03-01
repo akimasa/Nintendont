4d06dccb5bcccaa56c6c7efb7bb3a4df9e9ee4bd
Akimasa Tateba <akimasa@gmail.com>
2019-03-01 19:02:48 +0900

    install zip

--- travis.sh
+++ travis.sh
@@ -2,6 +2,8 @@ export DEVKITPRO=/opt/devkitpro
 export DEVKITARM=/opt/devkitpro/devkitARM
 export DEVKITPPC=/opt/devkitpro/devkitPPC
 dkp-pacman -Syyu --noconfirm 3ds-dev nds-dev gp32-dev gba-dev nds-portlibs
+apt update
+apt install zip
 cd /root/kernel/bin2h/
 bash make.sh
 cd /root/

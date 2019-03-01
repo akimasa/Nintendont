3191be3d54ce19f0d55ace55ae735df57c093ee3
Akimasa Tateba <akimasa@gmail.com>
2019-03-01 19:06:40 +0900

    apt-get

--- travis.sh
+++ travis.sh
@@ -1,9 +1,9 @@
 export DEVKITPRO=/opt/devkitpro
 export DEVKITARM=/opt/devkitpro/devkitARM
 export DEVKITPPC=/opt/devkitpro/devkitPPC
-dkp-pacman -Syyu --noconfirm 3ds-dev nds-dev gp32-dev gba-dev nds-portlibs
-apt update
-apt install zip
+dkp-pacman -Syyu --noconfirm devkitARM
+apt-get update
+apt-get install -y zip
 cd /root/kernel/bin2h/
 bash make.sh
 cd /root/

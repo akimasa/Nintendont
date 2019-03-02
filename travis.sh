d8740dd2ffd723b46675c16d297a49f0feef2614
Akimasa Tateba <akimasa@gmail.com>
2019-03-02 10:58:37 +0900

    use prebuilt devkitpro docker image

--- travis.sh
+++ travis.sh
@@ -1,7 +1,3 @@
-export DEVKITPRO=/opt/devkitpro
-export DEVKITARM=/opt/devkitpro/devkitARM
-export DEVKITPPC=/opt/devkitpro/devkitPPC
-dkp-pacman -Syyu --noconfirm devkitARM
 apt-get update
 apt-get install -y zip
 cd /root/kernel/bin2h/
